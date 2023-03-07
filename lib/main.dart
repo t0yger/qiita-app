12294db14fe30279056986748345f5407804b2d5
t0yger <bb.osr_aim@me.com>
2023-03-07 16:49:31 +0900

    記事一覧を表示するようにした

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -7,40 +10,21 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Qiita App',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.lightGreen,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: '記事一覧'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -48,8 +32,23 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final List<String> entries = <String>['A', 'B', 'C'];
-  final List<int> colorCodes = <int>[600, 500, 100];
+  List items = [];
+
+  Future<void> getArticles() async {
+    var url =
+        Uri.https('qiita.com', 'api/v2/items', {'page': '1', 'per_page': '20'});
+    var response = await http.get(url);
+
+    setState(() {
+      items = jsonDecode(response.body);
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getArticles();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -59,38 +58,48 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: ListView.separated(
         padding: const EdgeInsets.all(8),
-        itemCount: entries.length,
+        itemCount: items.length,
         itemBuilder: (BuildContext context, int index) {
           return Container(
             height: 150,
             padding: const EdgeInsets.all(16),
             child: Row(
               children: [
-                const CircleAvatar(
+                CircleAvatar(
                     radius: 25,
-                    backgroundImage:
-                        NetworkImage('https://picsum.photos/200/300')),
+                    backgroundImage: NetworkImage(
+                        items[index]['user']['profile_image_url'])),
                 const SizedBox(width: 24),
                 Expanded(
                   child: Column(
                     children: [
                       Container(
                           alignment: Alignment.centerLeft,
-                          child: const Text(
-                            'article title',
-                            style: TextStyle(
+                          child: Text(
+                            items[index]['title'],
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 1,
+                            style: const TextStyle(
                                 fontWeight: FontWeight.bold, fontSize: 24),
                           )),
                       Container(
                           alignment: Alignment.centerLeft,
-                          child: const Text(
-                            'tags',
-                            style: TextStyle(color: Colors.grey),
+                          child: Text(
+                            items[index]['tags']
+                                .map((tag) => tag['name'])
+                                .join(','),
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 1,
+                            style: const TextStyle(color: Colors.grey),
                           )),
                       const SizedBox(height: 8),
                       Container(
                           alignment: Alignment.centerLeft,
-                          child: const Text('headline')),
+                          child: Text(
+                            items[index]['body'],
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 1,
+                          )),
                     ],
                   ),
                 )
